7d6c95c03417e36deb05bb413a92e13ced0e2fda
Jorge Rivera <jorgeman201@yahoo.com>
2020-08-07 13:49:46 -0600

    Now it can upload images to the server

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,19 +1,35 @@
+const path = require("path");
+const fs = require("fs-extra");
+
+const { randomNumber } = require("../helpers/libs");
+
 exports.getImage = (req, res) => {
   res.send("Image page");
 };
 
-exports.createImage = (req,res) => {
-    res.send('something');
-}
+exports.createImage = async (req, res) => {
+  const validExtensions = [".png", ".jpg", ".jpeg", ".gif"];
+  const ext = path.extname(req.file.originalname).toLocaleLowerCase();
+  if (!validExtensions.includes(ext)) {
+    console.log(`${ext} is not a valid format`);
+    console.log(validExtensions);
+    res.send("wrong type of file, please upload an image");
+    return;
+  }
+  const imgTempPath = req.file.path;
+  const targetPath = path.resolve(`src/public/upload/${randomNumber() + ext}`);
+  await fs.rename(imgTempPath, targetPath);
+  res.send("works");
+};
 
-exports.likeImage = (req,res) => {
-    res.send('Liking an image');
-}
+exports.likeImage = (req, res) => {
+  res.send("Liking an image");
+};
 
-exports.commentImage = (req,res) => {
-    res.send('commenting an image');
-}
+exports.commentImage = (req, res) => {
+  res.send("commenting an image");
+};
 
-exports.deleteImage = (req,res) => {
-    res.send('deleting an image');
-}
+exports.deleteImage = (req, res) => {
+  res.send("deleting an image");
+};
